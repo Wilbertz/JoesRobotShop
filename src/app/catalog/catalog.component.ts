4f6fcdc8d104e44b8e66fd3122237cfa5a5abc69
Harald Wilbertz <harald.wilbertz@gmx.de>
2024-09-02 09:18:22 +0200

    Work in progress.

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IProduct } from "./product.model";
+import {CartService} from "../cart.service";
 
 @Component({
   selector: 'bot-catalog',
@@ -10,7 +11,7 @@ export class CatalogComponent {
   products: IProduct[];
   filter: string = '';
 
-  constructor() {
+  constructor(private cartSvc: CartService) {
     this.products = [
       {
         id: 1,
@@ -191,6 +192,11 @@ export class CatalogComponent {
   getImageUrl(product: IProduct) : string
   {
     return '/assets/images/robot-parts/' + product.imageName;
+  }
+
+  addToCart(product: IProduct)
+  {
+    this.cartSvc.add(product)
   }
 
   getFilteredProducts()
