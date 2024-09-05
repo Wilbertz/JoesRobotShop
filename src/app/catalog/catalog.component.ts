78cf702f4d54fc5045864e316a62650609e997e3
Harald Wilbertz <harald.wilbertz@gmx.de>
2024-09-05 20:23:53 +0200

    Work in progress.

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { IProduct } from './product.model';
 import { CartService } from '../cart.service';
 import { ProductService} from "./product.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'bot-catalog',
@@ -14,19 +15,27 @@ export class CatalogComponent {
 
   constructor(
     private cartSvc: CartService,
-    private productSvc: ProductService) {
-  }
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.productSvc
       .getProducts()
       .subscribe(products => this.products = products);
+    this.route.queryParams.subscribe(params => {
+      this.filter = params['filter'] ?? '';
+    })
   }
+
   addToCart(product: IProduct) {
     this.cartSvc.add(product);
+    this.router.navigate(['/cart']);
   }
 
   getFilteredProducts() {
+    // this.filter = '';
     return this.filter === ''
       ? this.products
       : this.products.filter(
